edf97fe0f149b2cfa71ce4e71113ece552ac46b9
deelaurence <laurie143pelumi@gmail.com>
2024-08-20 16:52:38 +0100

    fix: homepage ui

--- post/urls.py
+++ post/urls.py
@@ -9,3 +9,10 @@ urlpatterns=[
     path('posts/category/<slug:category_slug>',cache_page(60 *60* 24)(BlogList.as_view()), name="posts_category"),
     path('post/<str:title>/', cache_page(60 *60* 24)(BlogPostDetailView.as_view()), name="post")
 ]
+# urlpatterns=[ 
+#     path('',  Home.as_view(), name="home"),
+#     path('posts/', BlogList.as_view(), name="posts"),
+#     path('add/',  ContentCreateView.as_view(), name="content_create"),
+#     path('posts/category/<slug:category_slug>',BlogList.as_view(), name="posts_category"),
+#     path('post/<str:title>/',BlogPostDetailView.as_view(), name="post")
+# ]
